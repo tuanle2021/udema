e2bff1b3058750621934617c08345a3d17222e93
tuanle2021 <hoangtuanle2021@gmail.com>
2021-01-19 21:01:33 +0700

    update filters

--- src/main/java/udema/filters/AdminFilter.java
+++ src/main/java/udema/filters/AdminFilter.java
@@ -1,6 +1,7 @@
 package udema.filters;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,6 +11,10 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import udema.constants.Constants;
+import udema.dao.models.User;
 
 @WebFilter("/admin/*")
 public class AdminFilter implements Filter {
@@ -18,7 +23,27 @@ public class AdminFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
-		// Code here
+		HttpSession session = request.getSession();
+		Object loginValue = session.getAttribute(Constants.CREDENTIALS);
+
+		if (loginValue == null || !(loginValue instanceof User)) {
+			response.sendRedirect("/login");
+			return;
+		}
+
+		User user = (User) loginValue;
+		if (!user.getStatus()) {
+			response.sendRedirect("/login");
+			return;
+		}
+
+		Integer roleId = user.getRoleId();
+		Boolean hasAccess = Arrays.asList(Constants.ROLE_ADMIN, Constants.ROLE_TEACHER).contains(roleId);
+		if (!hasAccess) {
+			response.sendRedirect("/login");
+			return;
+		}
+
 		chain.doFilter(request, response);
 	}
 }
